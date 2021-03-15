1547d375718624ae184994d00e422f3caf74b7de
woogi <kkjkkjcom1@gmail.com>
2021-03-15 16:19:39 +0900

    Updated: Firebase ios 추가, pod 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:brave_girls/controllers/bindings/authBinding.dart';
 import 'package:brave_girls/ui/splash_page.dart';
 import 'package:brave_girls/utils/pages.dart';
-import 'package:brave_girls/utils/root.dart';
 import 'package:fimber/fimber.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:get/get.dart';
 
