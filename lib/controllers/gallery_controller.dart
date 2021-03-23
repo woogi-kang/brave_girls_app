8ee4cea0561b4d5f93e9f968cdfdeb9365692125
woogi <kkjkkjcom1@gmail.com>
2021-03-23 16:00:49 +0900

    Updated: 구글 이미지 크롤링한거 리스트뷰 뿌리는거 추가

--- lib/controllers/gallery_controller.dart
+++ lib/controllers/gallery_controller.dart
@@ -1,4 +1,3 @@
-import 'package:fimber/fimber.dart';
 import 'package:get/get.dart';
 import 'package:web_scraper/web_scraper.dart';
 
@@ -6,23 +5,43 @@ class GalleryController extends GetxController {
   final String? query;
   GalleryController({this.query});
 
-  List<String>? _imagesPath;
+  List<String>? imagesPath = [];
+
+  bool _isModelReady = false;
+  bool get isModelReady => _isModelReady;
 
   @override
-  Future<void> onInit() async {
+  void onInit() async {
+    _isModelReady = false;
+    update();
+
     final webScrapper = WebScraper('https://www.google.com');
     if(await webScrapper.loadWebPage('/search?q=브레이브걸스&rlz=1C5CHFA_enKR942KR942&sxsrf=ALeKk02s7hC_gUksKF-q3y6EKCmrnv8Jww:1616464842429&source=lnms&tbm=isch&sa=X&ved=2ahUKEwituOXlqMXvAhUCMN4KHYyRC0EQ_AUoAXoECAEQAw&biw=1920&bih=976')) {
       List<Map<String, dynamic>> images = webScrapper.getElement('img"', ['src']);
+
+      if(images.isNotEmpty) {
+        for(var image in images) {
+          String url = image['attributes']['src'];
+          if(url.startsWith('https')) {
+            imagesPath!.add(url);
+          }
+        }
+      }
     }
+
+    _isModelReady = true;
+    update();
+    super.onInit();
   }
 
   @override
   void onReady() {
+    super.onReady();
 
   }
 
   @override
   void onClose() {
-
+    super.onClose();
   }
 }
--- lib/ui/member_detail/detail_gallery.dart
+++ lib/ui/member_detail/detail_gallery.dart
@@ -1,4 +1,5 @@
 import 'package:brave_girls/controllers/gallery_controller.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,7 +12,24 @@ class DetailGallery extends StatelessWidget {
     return GetBuilder<GalleryController>(
       init: GalleryController(query: query),
       builder: (controller) {
-        return SizedBox();
+        return !controller.isModelReady ? SizedBox() : GridView.builder(
+          itemCount: controller.imagesPath!.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              width: 80,
+              height: 80,
+              margin: EdgeInsets.all(5),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: CachedNetworkImage(
+                  imageUrl: controller.imagesPath![index],
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          },
+        );
       },
     );
   }
