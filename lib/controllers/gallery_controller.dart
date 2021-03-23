dd71bf7a65f30b87381abe791b4cc7b2f01d0623
woogi <kkjkkjcom1@gmail.com>
2021-03-23 14:36:19 +0900

    test: 네이버 크롤링 실패 (막힌듯) 구글 이미지로 변경 테스트 완료

--- lib/controllers/gallery_controller.dart
+++ lib/controllers/gallery_controller.dart
@@ -6,12 +6,13 @@ class GalleryController extends GetxController {
   final String? query;
   GalleryController({this.query});
 
-  List<Map<String,dynamic>>? _imagesPath;
+  List<String>? _imagesPath;
 
   @override
   Future<void> onInit() async {
-    final webScrapper = WebScraper('https://www.naver.com');
-    if(await webScrapper.loadWebPage('/search.naver?where=image&sm=tab_jum&query=$query')) {
+    final webScrapper = WebScraper('https://www.google.com');
+    if(await webScrapper.loadWebPage('/search?q=브레이브걸스&rlz=1C5CHFA_enKR942KR942&sxsrf=ALeKk02s7hC_gUksKF-q3y6EKCmrnv8Jww:1616464842429&source=lnms&tbm=isch&sa=X&ved=2ahUKEwituOXlqMXvAhUCMN4KHYyRC0EQ_AUoAXoECAEQAw&biw=1920&bih=976')) {
+      List<Map<String, dynamic>> images = webScrapper.getElement('img"', ['src']);
     }
   }
 
