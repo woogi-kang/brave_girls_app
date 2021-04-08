6ef1760a63c6a5f6e887622a1db7e1e1e4429f07
woogi <kkjkkjcom1@gmail.com>
2021-04-08 12:36:59 +0900

    스케줄 조정

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:get/get.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,11 +17,11 @@ void main() async {
   Fimber.plantTree(defaultFormat);
   await Firebase.initializeApp();
 
-  runApp(
+  initializeDateFormatting("ko").then((_) => runApp(
     Phoenix(
       child: BraveGirlsApp(),
     ),
-  );
+  ));
 }
 
 class BraveGirlsApp extends StatelessWidget {
--- lib/ui/calendar/calendar.dart
+++ lib/ui/calendar/calendar.dart
@@ -1,5 +1,6 @@
 import 'package:cell_calendar/cell_calendar.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class CalendarPage extends StatefulWidget {
   @override
@@ -36,86 +37,100 @@ class _CalendarPageState extends State<CalendarPage> {
   Widget build(BuildContext context) {
     final _sampleEvents = sampleEvents();
     final cellCalendarPageController = CellCalendarPageController();
-    return CellCalendar(
-      cellCalendarPageController: cellCalendarPageController,
-      events: _sampleEvents,
-      daysOfTheWeekBuilder: (dayIndex) {
-        final labels = ["S", "M", "T", "W", "T", "F", "S"];
-        return Padding(
-          padding: const EdgeInsets.only(bottom: 4.0),
-          child: Text(
-            labels[dayIndex],
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
-            textAlign: TextAlign.center,
-          ),
-        );
-      },
-      monthYearLabelBuilder: (datetime) {
-        final year = datetime!.year.toString();
-        final month = datetime.month.monthName;
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 4),
-          child: Row(
-            children: [
-              const SizedBox(width: 16),
-              Text(
-                "$month  $year",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const Spacer(),
-              IconButton(
-                padding: EdgeInsets.zero,
-                icon: Icon(Icons.calendar_today),
-                onPressed: () {
-                  cellCalendarPageController.animateToDate(
-                    DateTime.now(),
-                    curve: Curves.linear,
-                    duration: Duration(milliseconds: 300),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: CellCalendar(
+                cellCalendarPageController: cellCalendarPageController,
+                events: _sampleEvents,
+                daysOfTheWeekBuilder: (dayIndex) {
+                  final labels = ["일", "월", "화", "수", "목", "금", "토"];
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 4.0),
+                    child: Text(
+                      labels[dayIndex],
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
                   );
                 },
-              )
-            ],
-          ),
-        );
-      },
-      onCellTapped: (date) {
-        final eventsOnTheDate = _sampleEvents.where((event) {
-          final eventDate = event.eventDate;
-          return eventDate.year == date.year && eventDate.month == date.month && eventDate.day == date.day;
-        }).toList();
-        showDialog(
-          context: context,
-          builder: (_) => AlertDialog(
-            title: Text(date.month.monthName + " " + date.day.toString()),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: eventsOnTheDate
-                  .map(
-                    (event) => Container(
-                      width: double.infinity,
-                      padding: EdgeInsets.all(4),
-                      margin: EdgeInsets.only(bottom: 12),
-                      color: event.eventBackgroundColor,
-                      child: Text(
-                        event.eventName,
-                        style: TextStyle(color: event.eventTextColor),
+                monthYearLabelBuilder: (datetime) {
+                  final year = datetime!.year.toString();
+                  var month = datetime.month;
+                  var month_formatted = "";
+
+                  month_formatted = "$month월";
+
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4),
+                    child: Row(
+                      children: [
+                        const SizedBox(width: 16),
+                        Text(
+                          "$year $month_formatted",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const Spacer(),
+                        IconButton(
+                          padding: EdgeInsets.zero,
+                          icon: Icon(Icons.calendar_today),
+                          onPressed: () {
+                            cellCalendarPageController.animateToDate(
+                              DateTime.now(),
+                              curve: Curves.linear,
+                              duration: Duration(milliseconds: 300),
+                            );
+                          },
+                        )
+                      ],
+                    ),
+                  );
+                },
+                onCellTapped: (date) {
+                  final eventsOnTheDate = _sampleEvents.where((event) {
+                    final eventDate = event.eventDate;
+                    return eventDate.year == date.year && eventDate.month == date.month && eventDate.day == date.day;
+                  }).toList();
+                  showDialog(
+                    context: context,
+                    builder: (_) => AlertDialog(
+                      title: Text(date.month.monthName + " " + date.day.toString()),
+                      content: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: eventsOnTheDate
+                            .map(
+                              (event) => Container(
+                                width: double.infinity,
+                                padding: EdgeInsets.all(4),
+                                margin: EdgeInsets.only(bottom: 12),
+                                color: event.eventBackgroundColor,
+                                child: Text(
+                                  event.eventName,
+                                  style: TextStyle(color: event.eventTextColor),
+                                ),
+                              ),
+                            )
+                            .toList(),
                       ),
                     ),
-                  )
-                  .toList(),
+                  );
+                },
+                onPageChanged: (firstDate, lastDate) {
+                  /// Called when the page was changed
+                  /// Fetch additional events by using the range between [firstDate] and [lastDate] if you want
+                },
+              ),
             ),
-          ),
-        );
-      },
-      onPageChanged: (firstDate, lastDate) {
-        /// Called when the page was changed
-        /// Fetch additional events by using the range between [firstDate] and [lastDate] if you want
-      },
+          ],
+        ),
+      ),
     );
   }
 }
